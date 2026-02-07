c19f3485ab2f1753a92ac7aa0cf306528ffec017
ji-circle <sp202401jw@gmail.com>
2026-02-07 20:45:30 +0900

    refactor : line정리

--- src/main/java/com/cau/artchive/entity/Comment.java
+++ src/main/java/com/cau/artchive/entity/Comment.java
@@ -9,8 +9,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment extends BaseTimeEntity {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long commentId;
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "post_id") private Post post;
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_dbid") private User user;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_dbid")
+    private User user;
     private String content;
 }
