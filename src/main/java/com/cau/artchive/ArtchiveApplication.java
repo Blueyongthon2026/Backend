5ec8c5182a4a513288c498491a924647431a1ce4
ji-circle <sp202401jw@gmail.com>
2026-02-07 17:04:59 +0900

    init

---
+++ src/main/java/com/cau/artchive/ArtchiveApplication.java
@@ -0,0 +1,13 @@
+package com.cau.artchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtchiveApplication.class, args);
+    }
+
+}
